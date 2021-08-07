fbc97505a9bd10a4f2448aff380b7a2d908d121f
tranquanghuy0801 <huytran.quang080199@gmail.com>
2021-08-07 22:12:45 +1000

    Building our React app

--- src/Routes.js
+++ src/Routes.js
@@ -2,16 +2,38 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NotFound from "./containers/NotFound";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import SignUp from "./containers/SignUp";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
-          <Home />
+        <Home />
       </Route>
-     <Route>
-          <NotFound />
-     </Route>
+      <UnauthenticatedRoute exact path="/login">
+        <Login />
+      </UnauthenticatedRoute>
+      <UnauthenticatedRoute exact path="/signup">
+        <SignUp />
+      </UnauthenticatedRoute>
+      <AuthenticatedRoute exact path="/settings">
+        <Settings />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/new">
+        <NewNote />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/:id">
+        <Notes />
+      </AuthenticatedRoute>
+      <Route>
+        <NotFound />
+      </Route>
     </Switch>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,18 +1,20 @@
 const config = {
-     s3: {
-       REGION: "us-east-1",
-       BUCKET: "notes-app-upload-harry",
-     },
-     apiGateway: {
-       REGION: "us-east-1",
-       URL: "https://w55yqa1nca.execute-api.us-east-1.amazonaws.com/prod",
-     },
-     cognito: {
-       REGION: "us-east-1",
-       USER_POOL_ID: "us-east-1_XL6kc9scr",
-       APP_CLIENT_ID: "78pbu8dkcircmetecc7pvcgfu4",
-       IDENTITY_POOL_ID: "us-east-1:e60ef7b4-4806-415c-9f9c-e69b57940c92",
-     },
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-upload-harry",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://w55yqa1nca.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_XL6kc9scr",
+    APP_CLIENT_ID: "78pbu8dkcircmetecc7pvcgfu4",
+    IDENTITY_POOL_ID: "us-east-1:e60ef7b4-4806-415c-9f9c-e69b57940c92",
+  },
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_gBRXF4eN3f9gXQVqkZiH266s",
 };
 
 export default config;
